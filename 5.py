5b3a53a97f80418211ce6a8b2ef83214557687f1
rohamdarban52-sys <rohamdarban52@gmail.com>
2025-12-04 12:48:52 +0330

    change 1

---
+++ 5.py
@@ -0,0 +1,27 @@
+import turtle as t
+t.color("#EE0A84")
+t.begin_fill()
+for i in range(40):
+    t.fd(100)
+    t.rt(130)
+t.end_fill()
+
+
+
+
+
+'''from time import sleep
+def rang(x,v):
+    t.speed(1000)
+    t.color(x)
+    for i in range(100):
+        t.fd(100)
+        t.rt(v)
+    sleep(2)
+rang('#B52C07',101)'''
+   
+
+
+
+
+
